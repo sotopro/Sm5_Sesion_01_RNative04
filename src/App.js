47d2682f66e0f522889a89aa7f974e2be9326483
Daniel Soto <dasg.1993@gmail.com>
2021-09-22 20:48:10 -0500

    [feature] - map elements & conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
 import './App.css';
 
-function App() {
+const emojiList = [
+  {
+    value: '👊',
+    name: 'punch'
+  },
+  {
+    value: '🤘',
+    name: 'rocking'
+  },
+  {
+    value: '👋',
+    name: 'greetings'
+  }
+]
+
+const isLoggedIn = true;
+
+const App = ()  => {
+  const greeting = "greeting";
+  const displayEmojiName = event => alert(event.target.id);
   return (
     <div className="container">
-      <h1>Hello World</h1>
+      <h1 id={greeting}>Hello World</h1>
+      <p>I am Wrinting JSX</p>
+      {isLoggedIn && (
+        <ul>
+        {emojiList.map(emoji => (
+          <li>
+            <button onClick={displayEmojiName}>
+              <span role="img" aria-label={emoji.name} id={emoji.name}>{emoji.value}</span>
+            </button>
+          </li>
+        ))}
+      </ul>
+      )}
     </div>
   );
 }
